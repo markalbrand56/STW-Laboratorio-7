fbb0099f824c22d999adcdbee525aafe9ff63896
Mark Albrand <mark.albrand5@gmail.com>
2023-03-26 16:13:50 -0600

    Button

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,12 +1,20 @@
+import React from "react"
+import PropTypes from "prop-types"
 import "./Button.css"
 
-const Button = (props) => 
-{
+function Button({ className, children }) {
     return (
         <div>
-            <button className={props.className}>{props.children}</button>
+            <button className={className} type="button">
+                {children}
+            </button>
         </div>
     )
 }
 
-export { Button }
+Button.propTypes = {
+    className: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+}
+
+export default Button
